17ca94e4ca6ba0dbe1752859f16ce6bb55a2e6be
xpydr <purirohan62@gmail.com>
2026-02-21 10:06:38 -0500

    Initial Commit

---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by xpydr on 2026-02-21.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
