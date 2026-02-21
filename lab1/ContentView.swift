9b492c33421d63dbd1ee14dd7523514b91a48311
xpydr <purirohan62@gmail.com>
2026-02-21 10:21:09 -0500

    feat: add isPrime helper function

--- lab1/ContentView.swift
+++ lab1/ContentView.swift
@@ -11,6 +11,22 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    private func isPrime(_ n: Int) -> Bool {
+        if n < 2 { return false }
+        if n == 2 { return true }
+        if n % 2 == 0 { return false }
+        
+        let limit = Int(Double(n).squareRoot())
+        var i = 3
+        
+        while i <= limit {
+            if n % i == 0 { return false }
+            i += 2
+        }
+        
+        return true
+    }
 }
 
 #Preview {
